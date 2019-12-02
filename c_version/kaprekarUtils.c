7bc96997655d627ce25139a91c6e4649139be0fc
liuai2637 <54672932+liuai2637@users.noreply.github.com>
2019-12-02 11:50:31 -0600

    Update kaprekarUtils.c
    change modulous to modulus because it was miss spelled on line 42

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -39,7 +39,7 @@ int isKaprekar(int n) {
   //for each possible "split" of the square...
   for(i=1; i<=numDigits; i++) {//change numberOfDigits to numDigits because it is the name that been declared on line 35
     //increase the modulus by a factor of 10
-    modulous *= 10;
+    modulus *= 10;
 
     //split the square into two parts
     first = square / modulus;
